527e6136f3552ce04daefd57e93e32852ac8890a
MiliDiazGrizzuti <milagros.diazgrizzuti@hotmail.com>
2024-03-20 22:39:59 -0300

    agrego cosas vistas en clase

--- src/components/students/students.component.ts
+++ src/components/students/students.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class StudentsComponent {
   title = 'Cursos';
+
+  hasError = false;
+
+  isDisabled = true;
+
+  students = ['Rick', 'Daryl', 'Michonne', 'Maggie', 'Glenn'];
+
+  example = [
+    {
+      name: 'Rick',
+      aprobado: true
+    },
+    {
+      name: 'Daryl',
+      aprobado: false
+    },
+    {
+      name: 'Michonne',
+      aprobado: true
+    },
+    {
+      name: 'Maggie',
+      aprobado: true
+    },
+    {
+      name: 'Glenn',
+      aprobado: false
+    }]
+
+  mostrarListado = false;
+
+  birthdayStudents = new Date(1995, 5, 28);
+
+  alternarError () {
+    this.hasError = !this.hasError;
+  }
+
+
 }
+
