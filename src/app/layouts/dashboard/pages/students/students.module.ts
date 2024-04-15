5676d23f25fe7cb8966ab2d43c295850d38e30f5
Milagros Grizzutti <mgrizzutti@HCJSM.local>
2024-04-15 18:13:00 -0300

    agrego botones angular material

--- src/app/layouts/dashboard/pages/students/students.module.ts
+++ src/app/layouts/dashboard/pages/students/students.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { StudentsRoutingModule } from './students-routing.module';
 import { StudentsComponent } from './students.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatTableModule} from '@angular/material/table';
-
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { StudentDialogComponent } from './components/student-dialog/student-dialog.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    StudentsComponent
+    StudentsComponent,
+    StudentDialogComponent
   ],
   imports: [
     CommonModule,
     StudentsRoutingModule,
     MatToolbarModule,
-    MatTableModule
+    MatTableModule,
+    MatDialogModule,
+    MatButtonModule
   ],
   exports: [
     StudentsComponent
